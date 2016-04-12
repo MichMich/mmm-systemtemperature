272fe472b7874cf5ef408c178c8b533e74d2ec86
Michael Teeuw <michael@xonaymedia.nl>
2016-04-12 11:47:53 +0200

    first setup

---
+++ mm-systemtemperature.js
@@ -0,0 +1,25 @@
+Module.register("mm-systemtemperature",{
+
+	defaults: {
+		prependString: 'System temperature: '
+	},
+
+	start: function() {
+		this.temperature = 'fetching ...';
+		this.sendSocketNotification('CONNECT');
+	},
+
+	socketNotificationReceived: function(notification, payload) {
+	    if (notification === 'TEMPERATURE') {
+	    	this.temperature = payload;
+	    	this.updateDom();
+	    }
+	}, 
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.innerHTML = this.config.prependString + this.temperature;
+		return wrapper;
+	}
+});
---
+++ node_helper.js
@@ -0,0 +1,24 @@
+var NodeHelper = require("node_helper");
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+
+module.exports = NodeHelper.create({
+	start: function function_name () {
+		var self = this;
+		setInterval(function() {
+			self.sendTemperature();
+		}, 1000);
+	},
+
+	sendTemperature: function() {
+		var self = this;
+		child = exec("/opt/vc/bin/vcgencmd measure_temp", function (error, stdout, stderr) {
+			if (error) {
+				console.log(error);
+				return;
+			}
+	  		self.sendSocketNotification('TEMPERATURE', stdout);
+		});
+	}
+});
