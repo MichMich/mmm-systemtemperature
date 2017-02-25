b81203a1cd6827687283943ff7ccd1909c9d68a6
Garrett Bartley <wgbartley@gmail.com>
2017-02-24 23:52:24 -0500

    Configurable unit of measurement

--- mmm-systemtemperature.js
+++ mmm-systemtemperature.js
@@ -4,6 +4,7 @@ Module.register("mmm-systemtemperature",{
 		prependString: 'System temperature: ',
 		updateInterval: 5000,
 		animationSpeed: 0,
+		unit: 'c'
 	},
 
 	start: function() {
@@ -21,6 +22,23 @@ Module.register("mmm-systemtemperature",{
 	// Override dom generator.
 	getDom: function() {
 		var wrapper = document.createElement("div");
+
+		// Do unit any conversions
+		if(this.temperature!='fetching ...' && this.config.unit.toLowerCase()!='c') {
+			if(this.config.unit.toLowerCase()=='f')
+				this.temperature = parseFloat(this.temperature) * 9 / 5 + 32;
+			else if(this.config.unit.toLowerCase()=='k')
+				this.temperature = parseFloat(this.temperature) - 273.15;
+
+			// Round off the temperature to 2 decimal places
+			this.temperature = Math.round(parseFloat(this.temperature*100))/100;
+		}
+
+
+		// Append &deg; + unit
+		if(this.temperature!='fetching ...')
+			this.temperature += '&deg;'+this.config.unit.toUpperCase();
+
 		wrapper.innerHTML = this.config.prependString + this.temperature;
 		return wrapper;
 	}
