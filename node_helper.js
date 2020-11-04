3458c716c198af5ea04a7383e48d978a7ec8de31
naveda89 <alvaro@naveda.me>
2020-11-04 19:22:53 +0100

    Use ES2015 syntax and avoid 'self' usage

--- node_helper.js
+++ node_helper.js
@@ -1,7 +1,6 @@
 var NodeHelper = require("node_helper");
 var exec = require('child_process').exec;
 
-
 module.exports = NodeHelper.create({
 	start: function() {
 		console.log("Starting node helper: " + this.name);
@@ -9,26 +8,21 @@ module.exports = NodeHelper.create({
 
 	// Subclass socketNotificationReceived received.
 	socketNotificationReceived: function(notification, payload) {
-		var self = this;
-
 		if (notification === 'CONFIG') {
 			this.config = payload;
-
-			setInterval(function() {
-				self.sendTemperature();
+			setInterval(() => {
+				this.sendTemperature();
 			}, this.config.updateInterval);
 		}
-		
 	},
 
 	sendTemperature: function() {
-		var self = this;
-		child = exec("/opt/vc/bin/vcgencmd measure_temp", function (error, stdout, stderr) {
+		exec("/opt/vc/bin/vcgencmd measure_temp", (error, stdout, stderr) => {
 			if (error) {
 				console.log(error);
 				return;
 			}
-		self.sendSocketNotification('TEMPERATURE', parseFloat(stdout.replace('temp=','')));
+			this.sendSocketNotification('TEMPERATURE', parseFloat(stdout.replace('temp=','')));
 		});
 	}
 });
